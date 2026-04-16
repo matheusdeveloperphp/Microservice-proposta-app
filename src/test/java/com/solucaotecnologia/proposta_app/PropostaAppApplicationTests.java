020d4758c4f9655c00d4d400ae687fb21958eddc
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2026-04-15 21:00:34 -0300

    Starting the microservice project.

---
+++ src/test/java/com/solucaotecnologia/proposta_app/PropostaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucaotecnologia.proposta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
